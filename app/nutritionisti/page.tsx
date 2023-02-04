b2a3c841adad39104625e789c36eb0cd8743bad1
kaapo-studio <contact@kaapo.studio>
2023-02-04 14:56:24 +0200

    in construction

--- app/nutritionisti/page.tsx
+++ app/nutritionisti/page.tsx
@@ -1,24 +1,28 @@
-import HeaderSearchANutritionist from '#/ui/search-a-pro/HeaderSearchANutritionist';
+// import HeaderSearchANutritionist from '#/ui/search-a-pro/HeaderSearchANutritionist';
+//
+// async function getNutritionistsData() {
+//   const res = await fetch('https://kaapo.fit/api/trainers');
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
+//
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch nutritionists data');
+//   }
+//
+//   return res.json();
+// }
+//
+// export default async function SearchNutritionistsPage() {
+//   const nutritionistsData = await getNutritionistsData();
+//   return (
+//     <>
+//       <HeaderSearchANutritionist nutritionists={nutritionistsData} />
+//     </>
+//   );
+// }
 
-async function getNutritionistsData() {
-  const res = await fetch('https://kaapo.fit/api/trainers');
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch nutritionists data');
-  }
-
-  return res.json();
-}
-
-export default async function SearchNutritionistsPage() {
-  const nutritionistsData = await getNutritionistsData();
-  return (
-    <>
-      <HeaderSearchANutritionist nutritionists={nutritionistsData} />
-    </>
-  );
+export default function page() {
+  return <h1>In construction</h1>;
 }
--- app/sali-de-antrenament/page.tsx
+++ app/sali-de-antrenament/page.tsx
@@ -1,24 +1,28 @@
-import HeaderSearchAGym from '#/ui/search-a-pro/HeaderSearchAGym';
+// import HeaderSearchAGym from '#/ui/search-a-pro/HeaderSearchAGym';
+//
+// async function getGymsData() {
+//   const res = await fetch('https://kaapo.fit/api/trainers');
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
+//
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch gyms data');
+//   }
+//
+//   return res.json();
+// }
+//
+// export default async function SearchGymsPage() {
+//   const gymsData = await getGymsData();
+//   return (
+//     <>
+//       <HeaderSearchAGym gyms={gymsData} />
+//     </>
+//   );
+// }
 
-async function getGymsData() {
-  const res = await fetch('https://kaapo.fit/api/trainers');
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch gyms data');
-  }
-
-  return res.json();
-}
-
-export default async function SearchGymsPage() {
-  const gymsData = await getGymsData();
-  return (
-    <>
-      <HeaderSearchAGym gyms={gymsData} />
-    </>
-  );
+export default function page() {
+  return <h1>In construction</h1>;
 }
